1e9f6b02a98d1c2b59b3f7da023c9fe143cf15a0
Daniel <danecko24@gmail.com>
2023-04-18 21:02:09 +0200

    minor refactor

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'http://localhost:8080' //your api URL
-    baseURL: 'https://localhost:8443' //your api URL
+    // baseURL: 'http://localhost:8080' //your api URL HTTP
+    baseURL: 'https://localhost:8443' //your api URL HTTPS
 });
 
 export default api;
